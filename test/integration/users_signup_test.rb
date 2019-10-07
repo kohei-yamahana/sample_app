79dbb74b5345709cb42b31532609eb0879fc47f5
EC2 Default User <ec2-user@ip-172-31-28-253.us-east-2.compute.internal>
2019-10-07 02:53:13 +0000

    Finish user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,40 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  
+  test "invaid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+     post signup_path, params: { user: {name: "Example User",
+                                email: "user@invalid",
+                                password:   "foo",
+                                password_confirmation: "bar"}}
+                                  # テストの意義：ユーザー登録が失敗した時に、間違えて登録されていないか
+  #               assert_no_differenceで実行前後のユーザー数をカウントし同数か
+  #               確かめている。
+    end
+    
+  assert_template 'users/new'
+  assert_select 'div#error_explanation'
+  assert_select 'div.alert'
+  # assert_select 'form[action ="/signup"]'
+  #意義："invaid signup information" をテストした結果、それに付随して動作して
+  # ほしい機能が実際動いているか確かめる。
+  end
+  
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: {name: "Example User",
+                                email: "user@example.com",
+                                password:   "password",
+                                password_confirmation: "password"} }
+      end
+      follow_redirect!
+      assert_template 'users/show'
+      assert_not flash.empty?
+    end
+  # test "the truth" do
+  #   assert true
+  # end
+end
