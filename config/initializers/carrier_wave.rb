efd80fcacbe62cc98600570fbbd710c7a73b28cb
EC2 Default User <ec2-user@ip-172-31-28-253.us-east-2.compute.internal>
2020-02-23 14:44:56 +0000

    Add user microposts2

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -8,5 +8,9 @@ if Rails.env.production?
             :aws_secret_access_key => ENV['S3_SECRET_KEY']
         }
         config.fog_directory  = ENV['S3_BUCKET']
+        config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
     end
+    
+    # 日本語ファイル名の設定
+    CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
 end
